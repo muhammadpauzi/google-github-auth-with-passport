4e5bb58235ae27896aae9dbdab8e2a5e83e28e30
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-11-13 20:22:51 +0700

    feat: apply login with github's account

--- config/passport.js
+++ config/passport.js
@@ -1,34 +1,43 @@
 const { User } = require('../models');
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GithubStrategy = require('passport-github2').Strategy;
 
 module.exports = (passport) => {
-    passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
-    }, async (accessToken, refreshToken, profile, done) => {
+    const callback = async (accessToken, refreshToken, profile, done) => {
         const newUser = {
             authId: profile.id,
             displayName: profile.displayName,
-            // firstName: profile.name.givenName,
-            // lastName: profile.name.familyName,
             image: profile.photos[0].value
         };
 
         try {
-            let user = (await User.findOne({
+            let user = await User.findOne({
                 where: { authId: profile.id },
-            })).toJSON();
+            });
 
             if (!user) {
                 user = await User.create(newUser);
             }
 
-            return done(null, user);
+            return done(null, user.toJSON());
         } catch (error) {
             console.log(error.message);
+            return done(error, null);
         }
-    }));
+    }
+
+    // google
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    }, callback));
+    // github
+    passport.use(new GithubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: '/auth/github/callback'
+    }, callback));
 
     passport.serializeUser((user, done) => {
         done(null, user.authId);
--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,16 @@ router.get('/google/callback',
         res.redirect('/dashboard');
     });
 
+router.get('/github',
+    passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/dashboard');
+    });
+
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
