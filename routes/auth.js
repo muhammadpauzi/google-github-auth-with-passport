a9137e44b494348f40fc8a6d9b7f7bfcafdfc116
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-11-13 20:41:04 +0700

    feat: force google login choose account

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const router = require('express').Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile'], prompt: 'select_account' }));
 
 router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
